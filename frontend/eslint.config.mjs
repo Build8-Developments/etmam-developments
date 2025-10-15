beda7803181bfbfdcea59ab58ee3364a0cd04141
Seif Aldeen Ashraf <177072202+seif-ashraf-007@users.noreply.github.com>
2025-10-15 23:09:27 +0300

    Disable '@typescript-eslint/no-explicit-any' rule
    Disabled the 'no-explicit-any' rule in ESLint configuration.

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -19,6 +19,10 @@ const eslintConfig = [
       "build/**",
       "next-env.d.ts",
     ],
+    rules: {
+      // 👇 This disables the "no-explicit-any" rule everywhere
+      "@typescript-eslint/no-explicit-any": "off",
+    },
   },
 ];
 
